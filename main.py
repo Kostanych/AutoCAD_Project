dd708cf1d7246ad003477469974c70d7095da1e1
konstantin <konstantin_sviblov@epam.com>
2021-06-23 15:33:02 +0300

    some refactoring

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ from AutocadProject.utils import files
 from AutocadProject.utils.data import get_dim_info
 
 dxf = files.open_dxf_file()
-print(type(dxf))
 df, entities, dimensions = get_dim_info(dxf)
 print(df)
